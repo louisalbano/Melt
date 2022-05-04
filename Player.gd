3c64ef1273a6099612efba4532ece1047e431c88
Doug Bunyea <dougbunyea@gmail.com>
2022-05-03 23:39:20 -0400

    added walljump movement delay

--- Player.gd
+++ Player.gd
@@ -6,17 +6,17 @@ var jump_speed = -1200
 var move_speed = 500
 const UP = Vector2(0,-1)
 var in_shadow = 0
+var ignore_control = false
+
+onready var control_timer = get_node("control_timer")
 onready var collision_ray = get_node("collision_ray")
 
 func _physics_process(delta):
-	var space_rid = get_world_2d().space
-	var space_state = Physics2DServer.space_get_direct_state(space_rid)
-	
 	velocity.y += delta * GRAVITY
 	
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") and !ignore_control:
 		velocity.x = move_speed
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") and !ignore_control:
 		velocity.x = -move_speed
 		
 	move_and_slide(velocity, UP)
@@ -25,15 +25,20 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = jump_speed
 			
-		if is_on_wall():
-			velocity.y = jump_speed
-			if collision_ray.is_colliding():
-				print("collision")
-				velocity.x = jump_speed + 10
-			else:
-				velocity.x = -jump_speed - 10
-			
-			
+		else:
+			if is_on_wall():
+				ignore_control = true
+				print("ignore control")
+				velocity.y = jump_speed
+				if collision_ray.is_colliding():
+					print("collision")
+					velocity.x = -1000
+				else:
+					velocity.x = 1000
+					
+				control_timer.start(.15)			
+	if control_timer.is_stopped():
+		ignore_control = false
 			
 	
 	velocity.x = lerp(velocity.x, 0, 0.06)
