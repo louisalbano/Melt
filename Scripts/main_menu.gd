569604d65256e413f6eb651d1b75f13671a02787
Louis Albano <louisalbano21@gmail.com>
2024-12-05 17:26:26 -0500

    add controller support for main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,12 +1,13 @@
 extends Control
 
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/Start.grab_focus()
+
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
 func _on_options_pressed():
 	pass # Replace with function body.
-
 
 func _on_quit_pressed():
 	get_tree().quit()
